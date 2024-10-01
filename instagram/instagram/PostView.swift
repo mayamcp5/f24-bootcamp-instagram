320718d03133b1cd2c959be2f191d942f7938640
mayamcp5 <mayamcp@icloud.com>
2024-10-01 14:35:44 -0400

    Add state and binding

--- instagram/instagram/PostView.swift
+++ instagram/instagram/PostView.swift
@@ -8,6 +8,11 @@
 import SwiftUI
 
 struct PostView: View {
+    @State private var likes = 0
+    @State private var liked = 0
+    @State private var marked = 0
+    @State private var likeButton: String = "heart"
+    @State private var bookmarkButton: String = "bookmark"
     var body: some View {
         VStack(alignment: .leading, spacing: 8) {
             HStack {
@@ -22,15 +27,37 @@ struct PostView: View {
                 .scaledToFit()
             VStack (alignment: .leading){
                 HStack(spacing: 16) {
-                    Image(systemName: "heart")
+                    Button {
+                        if liked == 0 {
+                            likeButton = "heart.fill"
+                            likes += 1
+                            liked = 1
+                        } else {
+                            likeButton = "heart"
+                            likes -= 1
+                            liked = 0
+                        }
+                    } label: {
+                        Image(systemName: likeButton).foregroundStyle(.black)
+                    }
                     Image(systemName: "message")
                     Image(systemName: "paperplane")
                     Spacer()
-                    Image(systemName: "bookmark")
+                    Button {
+                        if marked == 0 {
+                            bookmarkButton = "bookmark.fill"
+                            marked = 1
+                        } else {
+                            bookmarkButton = "bookmark"
+                            marked = 0
+                        }
+                    } label: {
+                        Image(systemName: bookmarkButton).foregroundStyle(.black)
+                    }
                 }
                 .font(.title2)
                 
-                Text("0 likes").font(.footnote).fontWeight(.semibold)
+                Text("\(likes) likes").font(.footnote).fontWeight(.semibold)
                 Text("**mayamcp** random stuff").font(.footnote)
                 Text("September 17").font(.caption).foregroundStyle(.secondary)
             }
